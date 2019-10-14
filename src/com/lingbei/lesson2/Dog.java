d99277e8c0ed336d03337233dba6f42826ee599d
lingbei <lingbei@wacai.com>
2019-10-14 13:12:51 +0800

    java study first commit

---
+++ src/com/lingbei/lesson2/Dog.java
@@ -0,0 +1,16 @@
+package com.lingbei.lesson2;
+
+public class Dog {
+    String beed;
+    int age;
+    String color;
+
+    void barking(){
+    }
+
+    void hungry(){
+    }
+
+    void sleeping(){
+    }
+}
---
+++ src/com/lingbei/lesson2/Puppy.java
@@ -0,0 +1,11 @@
+package com.lingbei.lesson2;
+
+public class Puppy {
+    public Puppy(String name){
+        System.out.println("小狗的名字是："+name);
+    }
+
+    public static void main(String[] args) {
+        Puppy puppy = new Puppy("tommy");
+    }
+}
